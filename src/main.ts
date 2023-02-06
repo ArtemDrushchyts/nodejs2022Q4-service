8ad7a15d0ecda7bdc468e768be926a7c69832241
Artem Drushchyts <artem.drushchyts@tangiblee.com>
2023-02-06 17:15:09 +0300

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
-import { resolve } from 'path';
+import { join, resolve } from 'path';
+import { parse } from 'yaml';
+import { readFile } from 'fs/promises';
+import { SwaggerModule } from '@nestjs/swagger';
 
 dotenv.config({ path: resolve(process.cwd(), '.env') });
 
 async function bootstrap() {
   const port = process.env.PORT || 4000;
   const app = await NestFactory.create(AppModule);
+  const doc = await readFile(join(__dirname, '..', 'doc', 'api.yaml'), 'utf-8');
+  SwaggerModule.setup('doc', app, parse(doc));
   await app.listen(port);
 }
 bootstrap();
